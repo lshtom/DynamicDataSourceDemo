de8be84716fbad91db65236434f77d2a16186e67
lshtom&林绍灏 <1545384472@qq.com>
2020-05-02 17:34:21 +0800

    增加用于测试的接口和模拟业务逻辑，用于测试读写分离的效果

--- src/main/java/com/github/lshtom/service/impl/UserServiveImpl.java
+++ src/main/java/com/github/lshtom/service/impl/UserServiveImpl.java
@@ -3,12 +3,14 @@ package com.github.lshtom.service.impl;
 import com.github.lshtom.dao.mapper.UserDAO;
 import com.github.lshtom.dao.model.User;
 import com.github.lshtom.service.UserServive;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author lshtom
@@ -41,5 +43,30 @@ public class UserServiveImpl implements UserServive {
 //    @DynamicDataSourceConfig("ds1")
     public User findById(Long id) {
         return userDAO.selectById(id);
+    }
+
+    private AtomicInteger testCount = new AtomicInteger(0);
+    @Override
+    @Transactional
+    public List<User> doSomething(User user) {
+        Date curDate = new Date();
+        // 写操作1
+        user.setCreateDate(curDate);
+        userDAO.insert(user);
+
+        // 写操作2
+        User user2 = new User();
+        BeanUtils.copyProperties(user, user2);
+        user2.setCreateDate(curDate);
+        user2.setName(user.getName()+"test2");
+        userDAO.insert(user2);
+
+        // 模拟发生异常
+        if (testCount.incrementAndGet() % 3 == 0) {
+            throw new RuntimeException("模拟事务中发生异常");
+        }
+
+        // 读操作
+        return userDAO.query();
     }
 }
